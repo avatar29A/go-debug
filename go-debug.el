4d63529ed51e1fdf966172cbd459a04c62a8a80f
Glebov Boris <avatar29A@gmail.com>
2016-11-26 21:00:32 +0500

    Inital commit

---
+++ go-debug.el
@@ -0,0 +1,57 @@
+;;; go-debug.el --- Mojer mode for the Go debugging
+
+;; Copyright 2016 The go-debug Authors. All rights reserved.
+;; Use of this source code is governed by a MIT-style
+;; license that can be found in the LICENSE file.
+
+;; Author: The go-debug Authors
+;; Version: 0.1.0
+;; Keywords: debug go
+;; Url: https://github.com/avatar29A/go-debug/
+;;
+;; This file is not part of GNU Emacs.
+
+;;; Code
+
+(require 'cl-lib)
+(require 'go-mode)
+
+(defvar path-to-dlv "/home/warlock/workspace/bin/dlv")
+(setq path-to-dlv "/home/warlock/workspace/bin/dlv1")
+
+(defvar gdg-version "0.1.0")
+(defvar gdg--dlv-is-running nil)
+
+(defun gdg--dlv-exists-p ()
+  "Check that path to dlv is exists"
+  (file-exists-p path-to-dlv))
+
+(defmacro dlv (&rest body)
+  "This macros wrap all code to check if dlv is exists in system."
+  (if (gdg--dlv-exists-p)
+      `(progn ,@body)
+    `(message "dlv is not found. Please check path-to-dlv (%s) variable." path-to-dlv)))
+
+(defun gdg-start (path-to-package)
+  "Start dlv as headless instance. Wait to connect to localhost:0")
+
+(defun gdg-debug (path)
+  "Compile and begin debugging main package in current directory, or the package specified.")
+
+(defun gdg-attach (pid)
+  "Attach to running process and begin debugging")
+
+(defun gdg-connect (host port)
+  "Connect to a headless debug server")
+
+(defun gdg-test (path-to-tests)
+  "Compile test binary and begin debugging program.")
+
+(defun gdg-trace (path-to-package)
+  "Compile and begin tracing program")
+
+(defun gdg-version ()
+  (dlv
+   (message "dlv (%s), go-debug (%s)", "0.11.0-alpha", gdg-version)))
+
+(provide 'go-debug)
